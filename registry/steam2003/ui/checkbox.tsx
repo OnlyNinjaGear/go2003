ae35ab05dfbccc03f0b596ea9793135efcde7e15
ninjagear <ninjagear.coop@gmail.com>
2026-05-01 00:32:07 +0700

    auto: checkbox.tsx

--- registry/steam2003/ui/checkbox.tsx
+++ registry/steam2003/ui/checkbox.tsx
@@ -1,10 +1,10 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { Check, Minus } from "lucide-react";
+import * as React from "react"
+import { CheckIcon, MinusIcon } from "lucide-react"
+import { Checkbox as CheckboxPrimitive } from "radix-ui"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 function Checkbox({
   className,
@@ -17,17 +17,21 @@ function Checkbox({
         "peer bevel-in size-[13px] shrink-0 cursor-default",
         "bg-input-bg text-input-fg",
         "outline-none focus-visible:win32-focus focus-visible:[outline-offset:1px]",
-        "disabled:bg-panel-pressed disabled:text-[var(--border-light)] disabled:cursor-default",
+        "disabled:bg-panel-pressed disabled:text-[var(--border-light)] disabled:pointer-events-none",
+        "aria-invalid:border-destructive",
         className
       )}
       {...props}
     >
-      <CheckboxPrimitive.Indicator className="group flex items-center justify-center">
-        <Check className="size-3 stroke-[2.5] group-data-[state=indeterminate]:hidden" />
-        <Minus className="size-3 stroke-[2.5] group-data-[state=checked]:hidden" />
+      <CheckboxPrimitive.Indicator
+        data-slot="checkbox-indicator"
+        className="group flex items-center justify-center"
+      >
+        <CheckIcon className="size-3 stroke-[2.5] group-data-[state=indeterminate]:hidden" />
+        <MinusIcon className="size-3 stroke-[2.5] group-data-[state=checked]:hidden" />
       </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
-  );
+  )
 }
 
-export { Checkbox };
+export { Checkbox }
