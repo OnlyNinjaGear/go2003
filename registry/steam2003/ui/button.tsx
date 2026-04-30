fe4dcd892b573f786c44883e56cf68a86ad5dcdb
ninjagear <ninjagear.coop@gmail.com>
2026-05-01 00:31:41 +0700

    auto: button.tsx

--- registry/steam2003/ui/button.tsx
+++ registry/steam2003/ui/button.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   [
-    "inline-flex items-center justify-center gap-1 whitespace-nowrap select-none cursor-default",
+    "inline-flex items-center justify-center gap-1 whitespace-nowrap select-none cursor-default shrink-0",
     "font-sans text-[11px] leading-none",
     "[&_svg]:pointer-events-none [&_svg]:size-3 [&_svg]:shrink-0",
     "outline-none focus-visible:win32-focus active:focus-visible:outline-none active:focus-visible:shadow-none",
