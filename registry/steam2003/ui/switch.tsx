4159292ff57d98cd251375c38c5bc97ceee47130
ninjagear <ninjagear.coop@gmail.com>
2026-05-01 17:48:18 +0700

    auto: switch.tsx

---
+++ registry/steam2003/ui/switch.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import * as React from "react"
+import { Switch as SwitchPrimitive } from "radix-ui"
+
+import { cn } from "@/lib/utils"
+
+function Switch({
+  className,
+  ...props
+}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+  return (
+    <SwitchPrimitive.Root
+      data-slot="switch"
+      className={cn(
+        "peer inline-flex h-5 w-9 shrink-0 items-center cursor-default select-none outline-none",
+        "bevel-in bg-input-bg",
+        "focus-visible:win32-focus",
+        "disabled:opacity-50 disabled:pointer-events-none",
+        className
+      )}
+      {...props}
+    >
+      <SwitchPrimitive.Thumb
+        data-slot="switch-thumb"
+        className="pointer-events-none block size-4 bevel-out bg-panel data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
+      />
+    </SwitchPrimitive.Root>
+  )
+}
+
+export { Switch }
