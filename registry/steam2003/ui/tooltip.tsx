7aa4dfce53c7fdd2f42dcd585702b39a1399370c
ninjagear <ninjagear.coop@gmail.com>
2026-05-01 17:37:38 +0700

    auto: tooltip.tsx

--- registry/steam2003/ui/tooltip.tsx
+++ registry/steam2003/ui/tooltip.tsx
@@ -32,8 +32,7 @@ function TooltipTrigger({
 
 function TooltipContent({
   className,
-  sideOffset = 0,
-  children,
+  sideOffset = 2,
   ...props
 }: React.ComponentProps<typeof TooltipPrimitive.Content>) {
   return (
@@ -42,14 +41,11 @@ function TooltipContent({
         data-slot="tooltip-content"
         sideOffset={sideOffset}
         className={cn(
-          "z-50 w-fit origin-(--radix-tooltip-content-transform-origin) animate-in rounded-md bg-foreground px-3 py-1.5 text-xs text-balance text-background fade-in-0 zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
+          "z-50 w-fit bevel-out bg-panel text-foreground px-1.5 py-0.5 font-sans text-[11px] outline-none select-none",
           className
         )}
         {...props}
-      >
-        {children}
-        <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-foreground fill-foreground" />
-      </TooltipPrimitive.Content>
+      />
     </TooltipPrimitive.Portal>
   )
 }
