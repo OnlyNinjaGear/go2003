42b6be25a38781bd9aaa11df0ffbde7d993fb3c4
ninjagear <ninjagear.coop@gmail.com>
2026-05-01 21:09:55 +0700

    auto: collapsible.tsx

---
+++ registry/steam2003/ui/collapsible.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Collapsible as CollapsiblePrimitive } from "radix-ui"
+
+function Collapsible({
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
+  return <CollapsiblePrimitive.Root data-slot="collapsible" {...props} />
+}
+
+function CollapsibleTrigger({
+  className,
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleTrigger>) {
+  return (
+    <CollapsiblePrimitive.CollapsibleTrigger
+      data-slot="collapsible-trigger"
+      className={cn("cursor-default select-none", className)}
+      {...props}
+    />
+  )
+}
+
+function CollapsibleContent({
+  className,
+  ...props
+}: React.ComponentProps<typeof CollapsiblePrimitive.CollapsibleContent>) {
+  return (
+    <CollapsiblePrimitive.CollapsibleContent
+      data-slot="collapsible-content"
+      className={cn("overflow-hidden", className)}
+      {...props}
+    />
+  )
+}
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }
