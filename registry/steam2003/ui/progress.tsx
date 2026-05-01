9cd154ed5f34f687cb13b450d656394cfe90a07a
ninjagear <ninjagear.coop@gmail.com>
2026-05-01 16:29:13 +0700

    auto: progress.tsx

--- registry/steam2003/ui/progress.tsx
+++ registry/steam2003/ui/progress.tsx
@@ -13,16 +13,13 @@ function Progress({
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
-      className={cn(
-        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
-        className
-      )}
+      className={cn("relative h-4 w-full overflow-hidden bevel-in bg-input-bg", className)}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="h-full w-full flex-1 bg-primary transition-all"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+        className="h-full bg-primary"
+        style={{ width: `${value ?? 0}%` }}
       />
     </ProgressPrimitive.Root>
   )
